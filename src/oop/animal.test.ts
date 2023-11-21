f0e446238eceb6e66e72acfa71f41c8f392b71b6
jerry <piyapongvisitsin@gmail.com>
2023-11-21 09:17:47 +0700

    encapsulation private,public

--- src/oop/animal.test.ts
+++ src/oop/animal.test.ts
@@ -3,12 +3,12 @@ import {Animal} from "./animal";
 describe('Animal', () => {
   it('should be able to eat', () => {
    // given
-   const animal = new Animal();
+   const animal = new Animal("dog");
 
     // when
    const eatBehaviour = animal.eat();
 
     // then
-    expect(eatBehaviour).toEqual('I can eat');
+    expect(eatBehaviour).toEqual('dog can eat');
   });
 });
--- src/oop/animal.ts
+++ src/oop/animal.ts
@@ -1,5 +1,40 @@
 export class Animal {
+    private breed: string = '';
+    // constructor ไว้สำหรับ initialize ค่าเริ่มต้นให้กับ class
+    constructor(bred: string) {
+        this.breed = bred;
+    }
+
     eat(): string {
-        return 'I can eat';
+        return `${this.breed} can eat`;
     }
 }
+
+
+// why we need to use protected (private)
+class Transaction {
+    private amount: number = 0;
+    constructor(amount: number) {
+        this.amount = amount;
+    }
+    getAmount(): number {
+        return this.amount;
+    }
+    deposit(amount: number): void {
+        this.amount += amount;
+    }
+    withdraw(amount: number): number {
+        if(amount > this.amount) throw new Error('amount is not enough');
+        this.amount -= amount;
+        return amount;
+    }
+}
+
+class Customer {
+    firstName: string = '';
+    lastName: string = '';
+    fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
